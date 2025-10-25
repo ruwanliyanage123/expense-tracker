126b16ae05030bfff15eb22668bac1d15707df56
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-10-25 13:51:59 +0530

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                sidebar: "oklch(51.8% 0.253 323.949)",
+            },
+        },
+    },
+    plugins: [],
+};
